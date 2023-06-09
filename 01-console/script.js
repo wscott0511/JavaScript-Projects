90fd22c07995643f47d062fc7bee4471cf5f528e
William <rscott.sites@gmail.com>
2023-06-09 15:13:13 -0700

    first commit

---
+++ 01-console/script.js
@@ -0,0 +1,17 @@
+console.log(100);
+
+console.log("Hello World")
+
+console.log(100, 'Hello', true)
+
+console.error("Alert")
+console.warn("Warn")
+console.table({name: "Brade", email: 'brad@gmail.com'})
+console.group('simple')
+console.log(x)
+console.error("Alert")
+console.warn("Warn")
+console.groupEnd()
+
+const styles = 'padding: 10px; background-color: white; color: greeen;'
+console.log('%cHello World', styles)
---
+++ 03-variables/script.js
@@ -0,0 +1,60 @@
+// Ways to declare a variable
+// var let const
+
+let firstName = 'John';
+let lastName = 'Doe';
+console.log(firstName, lastName);
+
+let age = 30;
+
+console.log(age);
+
+// Naming conventions
+// - Only letters, numbers, underscores and dollar signs
+// - Cant start with a number
+
+// Multi-Word Fromatting
+//firstName = camelCase
+// first_name = underscore
+// FirstName = FascalCase
+//firstname = lowercase
+
+// Re-assigning Variables
+
+age = 31;
+console.log(age)
+
+let score;
+score = 1;
+console.log(score)
+
+if (true)
+{
+    score = score + 1;
+}
+
+console.log(score);
+
+const x = 100;
+
+const arry = [1, 2, 3, 4]
+arry.push(5)
+console.log(arry)
+
+const person =
+{
+    name: 'Brad',
+}
+
+person.name = 'John'
+person.email = 'brad@gmail.com'
+
+console.log(person)
+
+// Declare mulitple values at once
+
+let a, b, c;
+
+const d = 10, e = 20, f = 30;
+
+console.log(d)
