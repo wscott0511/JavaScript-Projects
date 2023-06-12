7e9e920d3f8bab6dbbc9df8bc6b023676ed6cbed
William <rscott.sites@gmail.com>
2023-06-12 11:37:55 -0700

    Add Type Coercion and Operators

---
+++ 07-operators/script.js
@@ -0,0 +1,39 @@
+// 1. Arithmetic Operators
+
+let x
+
+x = 5 + 5
+x = 5 - 5
+x = 5 * 5
+x = 5 / 5
+x = 7 % 5
+
+// Concatenation
+x = 'Hello' + ' World'
+
+// Exponent
+x = 2 ** 3
+
+// Increment
+x = 1
+// x = x + 1
+x += 1
+
+// Decrement
+x -= 1
+
+// Assignment
+x = 10
+
+x += 5
+
+// Comparison Operators
+x = 2 == 4
+x = 2 === '2'
+
+x = 2 != 2
+x = 2 !== '2'
+x = 10 > 5
+x = 10 < 5
+
+console.log(x)
